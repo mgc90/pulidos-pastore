23ad25cbc45c2cace51dadba9706e565ef89e743
mgc90 <matiascorrea@protonmail.com>
2026-08-06 11:46:51 -0300

    .nojekyll, instrucciones de deploy para githubactions, routing con prefijo de base para imágenes

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,8 @@ import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://mgc90.github.io',
+  base: '/pulidos-pastore/',
   vite: {
     plugins: [tailwindcss()]
   },
